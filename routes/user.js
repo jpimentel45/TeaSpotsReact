892f9e96bf9a82b759b66dfc3bce31597b4b270b
jpimentel45 <jpimentel45@gmail.com>
2019-08-19 21:40:22 -0700

    things are working might want to delete Ogserver when you feel safe

---
+++ routes/user.js
@@ -0,0 +1,121 @@
+const express = require('express')
+const router = express.Router()
+const User = require('../database/models/user')
+const passport = require('../passport')
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+process.env.SECRET_KEY = 'secret'
+router.post('/', (req, res) => {
+    console.log('user signup');
+    console.log(req.body)
+    const today = new Date()
+    // const userData = {
+    //     first_name: req.body.first_name,
+    //     last_name: req.body.last_name,
+    //     email: req.body.email,
+    //     password: req.body.password,
+    //     created: today
+    // }
+
+    const { username, password } = req.body
+    // ADD VALIDATION
+    User.findOne({ username: username }, (err, user) => {
+        if (err) {
+            console.log('User.js post error: ', err)
+        } else if (user) {
+            console.log('Sorry, already a user with the username ')
+            res.json({
+
+                error: `Sorry, already a user with the username: ${username}`
+            })
+        }
+        else {
+            const newUser = new User({
+                username: username,
+                password: password
+            })
+            console.log("req.body.password")
+            console.log(req.body.password)
+            bcrypt.hash(req.body.password, 10, (err, hash) => {
+                newUser.password = hash
+                User.create(newUser)
+                    .then(user => {
+                        res.json({ status: user.email + 'Registered!' })
+                    })
+                    .catch(err => {
+                        res.send('error: ' + err)
+                    })
+            })
+        }
+    })
+})
+
+router.post(
+    '/login',
+    function (req, res, next) {
+        console.log('routes/user.js, login, req.body: ');
+        console.log(req.body)
+        next()
+    },
+    passport.authenticate('local'),
+    (req, res) => {
+        console.log('logged in', req.user);
+        var userInfo = {
+            _id: req.user._id,
+            email: req.user.username
+        };
+
+        let token = jwt.sign(userInfo, process.env.SECRET_KEY, {
+            expiresIn: '604800'
+        })
+
+        res.send(token);
+    }
+)
+
+router.get('/', (req, res, next) => {
+    console.log('===== user!! PATH /======')
+    console.log(req.user)
+    if (req.user) {
+        res.json({ user: req.user })
+    } else {
+        res.json({ user: null })
+    }
+})
+
+router.get('/Profile', checkAuthentication, (req, res, next) => {
+    console.log('===== user!!/Profile======')
+    console.log('===== user!!/Profile======')
+    console.log('===== user!!/Profile======')
+    // console.log(req)
+    console.log(req.user)
+    if (req.user) {
+        res.json({ user: req.user })
+    } else {
+        res.json({ user: null })
+    }
+})
+
+router.post('/logout', (req, res) => {
+    if (req.user) {
+        req.logout()
+        console.log("logging out")
+        res.send({ msg: 'logging out' })
+    } else {
+        res.send({ msg: 'no user to log out' })
+    }
+})
+
+function checkAuthentication(req, res, next) {
+    console.log("REQ.AUTHENTICATED")
+    console.log(req.isAuthenticated())
+    if (req.isAuthenticated()) {
+        //req.isAuthenticated() will return true if user is logged in
+        console.log("checkAuthentication SUCCESS")
+        next();
+    } else {
+        console.log("checkAuthentication FAILED")
+    }
+}
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,24 +1,84 @@
-const express = require("express");
+const express = require('express')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const session = require('express-session')
+const dbConnection = require('./database')
+const MongoStore = require('connect-mongo')(session)
+const passport = require('./passport');
+const app = express()
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
+const PORT = process.env.port || 5000;
 
-const app = express();
-
+//DB Config
+const db = require('./config/keys').mongoURI;
+mongoose.connect(db, { useNewUrlParser: true })
+	.then(() => console.log('MongoDB Connected!'))
+	.catch(err => console.log(err));
 const special = require('./routes/api/special');
+//use routes
+app.use('/api/special', special)
+
+// Route requires
+const user = require('./routes/user')
+
+// MIDDLEWARE
+app.use(morgan('dev'))
+app.use(
+	bodyParser.urlencoded({
+		extended: false
+	})
+)
+app.use(bodyParser.json())
+
+// Sessions
+app.use(
+	session({
+		secret: 'fraggle-rock', //pick a random string to make the hash that is generated secure
+		store: new MongoStore({ mongooseConnection: dbConnection }),
+		resave: false, //required
+		saveUninitialized: false //required
+	})
+)
+
+//======================================================
+//                Juan
+//======================================================
+
+
+//const express = require("express");
+
+//const bodyParser = require("body-parser");
+
+//const app = express();
+
+
 //body parser middleware
 app.use(bodyParser.json());
 
 //DB Config
-const db = require('./config/keys').mongoURI;
-mongoose.connect(db, { useNewUrlParser: true})
-.then(()=> console.log('MongoDB Connected!'))
-.catch(err => console.log(err));
+//const db = require('./config/keys').mongoURI;
+mongoose.connect(db, { useNewUrlParser: true })
+	.then(() => console.log('MongoDB Connected!'))
+	.catch(err => console.log(err));
 
 //use routes
 app.use('/api/special', special)
-const port = process.env.port || 5000;
-app.listen(port, ()=> console.log(`Server started on port ${port}`));
-// mongoose.connect('mongodb://localhost/TeaSpotsMERN', (err)=>{
-//     if (err) throw err;
-//     console.log("Successfully connected")
-// });
+
+//======================================================
+//                /Juan
+//======================================================
+
+
+// Passport
+app.use(passport.initialize())
+app.use(passport.session()) // calls the deserializeUser
+
+app.use((req, res, next) => {
+	console.log('req.session', req.session);
+	return next();
+});
+// Routes
+app.use('/user', user)
+
+// Starting Server 
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
